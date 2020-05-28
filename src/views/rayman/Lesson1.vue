1f1a5bcbafff52d8335a88d12f29c7f49d6d21de
ZTjack <jack.jiang.scivan@gmail.com>
2020-05-28 16:20:23 +0800

    update

--- src/views/rayman/Lesson1.vue
+++ src/views/rayman/Lesson1.vue
@@ -2,11 +2,11 @@
  * @Author: Jack
  * @Date: 2020-05-28 16:03:54
  * @LastEditors: Jack
- * @LastEditTime: 2020-05-28 16:08:23
+ * @LastEditTime: 2020-05-28 16:19:22
  * @Description: 
 -->
 <template>
   <div class="about">
-    <h1>This is an Lesson1</h1>
+    <h1>This is an Lssesson1</h1>
   </div>
 </template>
