110f1f24c0af3827c3f2523bce9ff2f59116f3d5
KMMoonlight <kmmoonlight@outlook.com>
2020-06-12 16:02:36 +0800

    account_choose

--- src/views/kmmoonlight/AccountChoose.vue
+++ src/views/kmmoonlight/AccountChoose.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div ref="container">
-      <div class="account" @click="expandAccountList = !expandAccountList">
+      <div
+        class="account"
+        @click="expandAccountList = !expandAccountList"
+        :class="{ 'high-light': expandAccountList }"
+      >
         <div class="info">
           <div>{{ nowAccount }}</div>
           <div>{{ nowChildAccount }}</div>
@@ -15,9 +19,15 @@
           v-for="(item, index) in accountList"
           :key="item.type"
           class="account-item"
-          @click="chooseChildAccount(index)"
+          @click="chooseAccount(index, false)"
         >
-          {{ item.type }}
+          <div class="account-info">
+            {{ item.type }}
+          </div>
+
+          <div v-show="item.hasChild" style="margin-right:10px">
+            &gt;
+          </div>
         </div>
       </div>
       <div
@@ -28,13 +38,14 @@
           type="text"
           style="width:192px; height:30px"
           v-model="filterText"
+          placeholder="请输入关键字搜索"
         />
         <div class="child-item-list">
           <div
-            v-for="item in getChildrenList"
+            v-for="(item, index) in getChildrenList"
             :key="item"
             class="account-item"
-            @click="nowChildAccount = item"
+            @click="chooseAccount(index, true)"
           >
             {{ item }}
           </div>
@@ -51,84 +62,103 @@ export default {
     return {
       accountList: [
         {
-          type: 'a',
+          type: '币币账户',
           children: []
         },
         {
-          type: 'b',
+          type: '币币杠杆账户',
           children: [
-            'account1',
-            'account2',
-            'account3',
-            'account4',
-            'account5',
-            'account6',
-            'account7'
+            'LTC/BTC',
+            'ETH/BTC',
+            'ETC/BTC',
+            'BTC/USDT',
+            'QTUM/BTC',
+            'HC/BTC',
+            'XRP/BTC'
           ]
         },
         {
-          type: 'c',
+          type: '法币账户',
           children: ['accountA', 'accountB', 'accountC']
         },
         {
-          type: 'd',
+          type: '交割合约账户',
           children: []
         },
         {
-          type: 'e',
+          type: '余币宝',
           children: []
         },
         {
-          type: 'f',
+          type: '永续合约账户',
           children: []
         },
         {
-          type: 'g',
+          type: '借贷账户',
           children: []
         }
       ],
       expandAccountList: false,
       expandChildAccountList: false,
-      nowAccount: '',
+      nowAccount: '币币账户',
       nowChildAccount: '',
       filterText: ''
     }
   },
   methods: {
-    chooseChildAccount: function(index) {
-      this.expandChildAccountList = this.accountList[index].children.length > 0
-      if (this.nowAccount != this.accountList[index].type) {
-        this.nowChildAccount = ''
+    chooseAccount: function(index, isChild) {
+      if (isChild) {
+        this.nowChildAccount = this.getChildrenList[index]
+        this.clearState()
+      } else {
+        this.expandChildAccountList = this.accountList[index].hasChild
+        if (this.nowAccount != this.accountList[index].type) {
+          this.nowChildAccount = ''
+        }
+        this.nowAccount = this.accountList[index].type
+        if (!this.expandChildAccountList) {
+          this.clearState()
+        }
       }
-      this.nowAccount = this.accountList[index].type
-      this.filterText = ''
     },
     addOutSideClickListener: function(element) {
       document.addEventListener('click', event => {
         if (!element.contains(event.target)) {
-          this.expandAccountList = false
-          this.expandChildAccountList = false
-          this.filterText = ''
+          //debugger
+          this.clearState()
         }
       })
+    },
+    clearState: function() {
+      this.expandAccountList = false
+      this.expandChildAccountList = false
+      this.filterText = ''
     }
   },
   mounted() {
     this.addOutSideClickListener(this.$refs.container)
+
+    //给数组添加一个children是否大于0的标识
+    this.accountList = this.accountList.map(item => {
+      return {
+        ...item,
+        hasChild: item.children.length > 0
+      }
+    })
   },
   computed: {
     arrow: function() {
       return this.expandAccountList ? '↑' : '↓'
     },
     getChildrenList: function() {
-      for (let account of this.accountList) {
-        if (account.type === this.nowAccount) {
-          return account.children.filter(item => {
-            return item.indexOf(this.filterText) != -1
+      const account = this.accountList.find(
+        item => item.type === this.nowAccount
+      )
+      return account
+        ? account.children.filter(item => {
+            return item.toUpperCase().includes(this.filterText.toUpperCase())
           })
-        }
-      }
-      return []
+        : []
     }
   }
 }
@@ -141,6 +171,9 @@ export default {
   border: 1px solid #000000;
   display: flex;
   align-items: center;
+  /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px; */
 }
 
 .account-list {
@@ -149,20 +182,47 @@ export default {
   top: 100px;
   height: 324px;
   overflow: auto;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  /* border: 0.5px solid gray; */
 }
 
 .account-item {
   height: 80px;
-  border: 1px solid #000;
+  /* border: 1px solid #000; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.account-item:hover {
+  background-color: lightblue;
+}
+
+.account-info {
+  flex-grow: 1;
+}
+
+.account-list:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 
 .child-account-list {
   position: absolute;
   left: 220px;
-  top: 40px;
+  top: 50px;
   width: 200px;
   display: flex;
   flex-direction: column;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  /* border: 0.5px solid gray; */
+}
+
+.child-account-list:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 
 .info {
@@ -170,11 +230,15 @@ export default {
 }
 
 .child-item-list {
-  height: 324px;
+  max-height: 324px;
   overflow: auto;
 }
 
 .arrow {
   width: 20px;
+}
+
+.high-light {
+  border: 2px solid blue;
 }
 </style>
